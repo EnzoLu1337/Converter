ce0bf0376d723bda0f37012b393310770130f5d2
EnzoLu1337 <61724474+EnzoLu1337@users.noreply.github.com>
2024-04-22 08:31:37 +0400

    Добавление онлайн курса валют

--- Converter/Form1.Designer.cs
+++ Converter/Form1.Designer.cs
@@ -31,11 +31,11 @@
             this.components = new System.ComponentModel.Container();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label13 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.CursLabel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.currencyCombo = new System.Windows.Forms.ComboBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.comboBox11 = new System.Windows.Forms.ComboBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -72,6 +72,7 @@
             this.comboBox6 = new System.Windows.Forms.ComboBox();
             this.label12 = new System.Windows.Forms.Label();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.FinalCurseLabel = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -97,11 +98,12 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.FinalCurseLabel);
+            this.tabPage1.Controls.Add(this.label13);
             this.tabPage1.Controls.Add(this.textBox1);
-            this.tabPage1.Controls.Add(this.label2);
-            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.CursLabel);
             this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.currencyCombo);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -110,31 +112,32 @@
             this.tabPage1.Text = "Валюта";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(61, 29);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(33, 13);
+            this.label13.TabIndex = 5;
+            this.label13.Text = "Пара";
+            // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(172, 45);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
-            // label2
+            // CursLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(447, 48);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(296, 44);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 21);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Конвертировать ->";
-            this.button1.UseVisualStyleBackColor = true;
+            this.CursLabel.AutoSize = true;
+            this.CursLabel.Location = new System.Drawing.Point(61, 78);
+            this.CursLabel.Name = "CursLabel";
+            this.CursLabel.Size = new System.Drawing.Size(37, 13);
+            this.CursLabel.TabIndex = 3;
+            this.CursLabel.Text = "Курс: ";
             // 
             // label1
             // 
@@ -145,13 +148,14 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Значение";
             // 
-            // comboBox1
+            // currencyCombo
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(27, 45);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 0;
+            this.currencyCombo.FormattingEnabled = true;
+            this.currencyCombo.Location = new System.Drawing.Point(27, 45);
+            this.currencyCombo.Name = "currencyCombo";
+            this.currencyCombo.Size = new System.Drawing.Size(121, 21);
+            this.currencyCombo.TabIndex = 0;
+            this.currencyCombo.SelectedIndexChanged += new System.EventHandler(this.currencyCombo_SelectedIndexChanged);
             // 
             // tabPage2
             // 
@@ -556,6 +560,15 @@
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
+            // FinalCurseLabel
+            // 
+            this.FinalCurseLabel.AutoSize = true;
+            this.FinalCurseLabel.Location = new System.Drawing.Point(311, 48);
+            this.FinalCurseLabel.Name = "FinalCurseLabel";
+            this.FinalCurseLabel.Size = new System.Drawing.Size(43, 13);
+            this.FinalCurseLabel.TabIndex = 6;
+            this.FinalCurseLabel.Text = "Итого: ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -592,10 +605,9 @@
         private System.Windows.Forms.TabPage tabPage5;
         private System.Windows.Forms.TabPage tabPage6;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox currencyCombo;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label CursLabel;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label4;
@@ -627,6 +639,8 @@
         private System.Windows.Forms.ComboBox comboBox9;
         private System.Windows.Forms.ComboBox comboBox8;
         private System.Windows.Forms.ComboBox comboBox7;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label FinalCurseLabel;
     }
 }
 
--- Converter/Form1.cs
+++ Converter/Form1.cs
@@ -3,23 +3,94 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace Converter
 {
     public partial class Form1 : Form
     {
+
+        string url = "https://currate.ru/api/";
+        string apiKey = "4e2ffd1d6177270f8d59fe3ca54f00d5";
+
+        CurrencyPairs[] pairsArray = { };
+        struct CurrencyPairs
+        {
+            public string Pair;
+            public double Course;
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private async void Form1_Load(object sender, EventArgs e)
         {
+            string currencyResponse = await Get($"{url}?get=currency_list&key={apiKey}");
+            if (!string.IsNullOrEmpty(currencyResponse))
+            {
+                //MessageBox.Show($"currencyResponse: {currencyResponse}");
 
+                string[] currencyList = GetCurrencyList(currencyResponse);
+
+                //MessageBox.Show($"currencyList: {currencyList.Length}");
+
+                // Формируем строку с перечислением всех валютных пар из currencyList
+                string pairs = string.Join(",", currencyList);
+
+                // Получаем курсы для всех валютных пар
+                string ratesResponse = await Get($"{url}?get=rates&pairs={pairs}&key={apiKey}");
+                //MessageBox.Show($"ratesResponse: {ratesResponse}");
+
+                if (!string.IsNullOrEmpty(ratesResponse))
+                {
+                    // Парсим ответ JSON
+                    JObject jsonObject = JObject.Parse(ratesResponse);
+                    JObject dataObject = (JObject)jsonObject["data"];
+
+                    // Создаем список для хранения объектов CurrencyPairs
+                    List<CurrencyPairs> pairsList = new List<CurrencyPairs>();
+
+                    // Заполняем список объектами CurrencyPairs
+                    foreach (var pair in dataObject)
+                    {
+                        CurrencyPairs currencyPair = new CurrencyPairs();
+                        currencyPair.Pair = pair.Key;
+                        if (double.TryParse(pair.Value.ToString(), NumberStyles.Float, CultureInfo.InvariantCulture, out currencyPair.Course))
+                        {
+                            pairsList.Add(currencyPair);
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Ошибка парсинга курса для валютной пары {pair.Key}: {pair.Value}");
+                        }
+                    }
+
+                    pairsArray = pairsList.ToArray();
+                    //MessageBox.Show($"Количество валютных пар в массиве: {pairsArray.Length}");
+                    foreach (var pair in pairsArray)
+                    {
+                        currencyCombo.Items.Add(pair.Pair);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Пустой ответ при загрузке курсов.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Пустой ответ при получении списка валютных пар.");
+            }
         }
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
@@ -32,5 +103,76 @@ namespace Converter
             }
         }
 
+        static async Task<string> Get(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    return await response.Content.ReadAsStringAsync();
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Ошибка при выполнении запроса: {e.Message}");
+                    return string.Empty;
+                }
+            }
+        }
+
+        static string[] GetCurrencyList(string jsonResponse)
+        {
+            JObject jsonObject = JObject.Parse(jsonResponse);
+            JArray dataArray = (JArray)jsonObject["data"];
+            return dataArray.ToObject<string[]>();
+        }
+
+        private void currencyCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedCurrencyPair = currencyCombo.SelectedItem.ToString();
+            CurrencyPairs selectedPair = pairsArray.FirstOrDefault(pair => pair.Pair == selectedCurrencyPair);
+
+            if (!string.IsNullOrEmpty(selectedPair.Pair))
+            {
+                double rate = selectedPair.Course;
+                CursLabel.Text = $"Курс {selectedCurrencyPair}: {rate}";
+            }
+            else
+            {
+                CursLabel.Text = "Курс не найден";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string selectedCurrencyPair = currencyCombo.SelectedItem.ToString();
+            CurrencyPairs selectedPair = pairsArray.FirstOrDefault(pair => pair.Pair == selectedCurrencyPair);
+
+            double count = double.Parse(textBox1.Text);
+            if (selectedCurrencyPair != null)
+            {
+                if (selectedPair.Pair != null)
+                {
+                    if (double.TryParse(selectedPair.Course.ToString(), out double course))
+                    {
+                        double total = count * course;
+                        FinalCurseLabel.Text = $"Итого: {total}";
+                    }
+                    else
+                    {
+                        FinalCurseLabel.Text = "Ошибка при парсинге курса";
+                    }
+                }
+                else
+                {
+                    FinalCurseLabel.Text = "Курс не найден";
+                }
+            }
+            else
+            {
+                FinalCurseLabel.Text = "Валютная пара не выбрана";
+            }
+        }
     }
 }
